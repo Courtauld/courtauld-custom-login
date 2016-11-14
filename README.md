12f6c20a4cab01563a72412368c6cc55fa1bd899
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2016-11-14 12:17:30 +0000

    Adds existing codebase

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # courtauld-custom-login
+
 A WordPress plugin to customise the login page according to The Courtauld's style guide. Works independently of the installed theme.
---
+++ courtauld_custom_login.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+   Plugin Name: Courtuald Custom Login
+   Plugin URI: http://example.org
+   Description: This plugin customises the login page according to the styles of the Courtauld websites.
+   Version: 1.0
+   Author: Jacob Charles Wilson
+   Author URI: https://jclwilson.github.io
+   License: GPL2
+   */
+
+// Custom link
+function courtauld_url_login(){
+	return get_bloginfo( 'url' ); // your URL here
+}
+add_filter('login_headerurl', 'courtauld_url_login');
+
+// Custom logo
+function login_css() {
+	wp_enqueue_style( 'login_css', plugins_url('login.css', __FILE__));
+}
+add_action('login_enqueue_scripts', 'login_css');
+
+// Custom Logo title
+function courtauld_logo_url_title() {
+    return get_bloginfo( 'title' );;
+}
+add_filter( 'login_headertitle', 'courtauld_logo_url_title' );
+
+// Custom WordPress Footer
+function remove_footer_admin () {
+
+}
+add_filter('admin_footer_text', 'remove_footer_admin');
+
+// Remove visual error shake
+function my_login_head() {
+remove_action('login_head', 'wp_shake_js', 12);
+}
+add_action('login_head', 'my_login_head');
+
+?>
